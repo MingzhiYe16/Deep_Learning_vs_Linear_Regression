a67b387bcf5ae6575b03be9c49b7032af117280f
MingzhiYe16 <1626384862@qq.com>
2024-03-20 21:19:28 -0700

    change number of epoch in training T5

--- Train_T5.py
+++ Train_T5.py
@@ -72,7 +72,7 @@ model = model.to(device)
 
 optimizer = AdamW(model.parameters(), lr=5e-5)
 
-num_epochs = 20
+num_epochs = 200
 num_training_steps = num_epochs * len(train_dataloader)
 lr_scheduler = get_scheduler("linear", optimizer=optimizer, num_warmup_steps=0, num_training_steps=num_training_steps)
 
